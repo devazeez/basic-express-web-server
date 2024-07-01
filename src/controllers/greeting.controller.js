338a8b4e70e42bd913a7f90037a11a6b8cd33fb0
Azeez <l.adeosun.la@gmail.com>
2024-07-01 05:13:23 +0100

    Initial commit, hello API

--- src/controllers/greeting.controller.js
+++ src/controllers/greeting.controller.js
@@ -6,6 +6,9 @@ import {
 export const greeting = async (req, res, next) => {
   try {
     const visitor_name = req.query.visitor_name;
+    const userIP =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log(userIP);
 
     if (!visitor_name) {
       return res.status(400).json({
@@ -19,9 +22,10 @@ export const greeting = async (req, res, next) => {
     );
 
     return res.status(200).json({
-        client_ip: visitorLocationDetails.ipAddress,
-        location: visitorLocationDetails.location.region,
-        greeting: `Hello, ${visitor_name}!, the temperature is ${visitorWeatherDetails.temperature} degrees Celcius in ${visitorLocationDetails.location.region}`,
+      client_ip: visitorLocationDetails.ipAddress,
+      location: visitorLocationDetails.location.region,
+      greeting: `Hello, ${visitor_name}!, the temperature is ${visitorWeatherDetails.temperature} degrees Celcius in ${visitorLocationDetails.location.region}`,
+      userIP
     });
   } catch (error) {
     next(error);
